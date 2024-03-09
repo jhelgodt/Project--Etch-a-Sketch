a2722b5d6b8649289f123396bb45aa5e0509eb94
jhelgodt <joakim.helgodt@gmail.com>
2024-03-09 16:35:05 +0100

    added for sketch function

--- javascript.js
+++ javascript.js
@@ -3,5 +3,8 @@ const container = document.getElementById("container");
 for (let i = 0; i < 16 * 16; i++) {
   const div = document.createElement("div");
   div.classList.add("grid-item");
+  div.addEventListener("mouseover", function () {
+    div.style.backgroundColor = "black"; // change this to any color you want
+  });
   container.appendChild(div);
 }
