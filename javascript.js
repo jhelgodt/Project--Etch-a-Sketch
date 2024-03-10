0eb5bb1019408618e7913d6c918e981318aafae5
jhelgodt <joakim.helgodt@gmail.com>
2024-03-10 10:57:22 +0100

    random coloring

--- javascript.js
+++ javascript.js
@@ -15,9 +15,16 @@ function createGrid(size) {
     div.dataset.alpha = 0; // Initialize alpha value
 
     div.addEventListener("mouseover", function () {
+      // Generate random RGB values
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+
       // Increase alpha by 10% (0.1), but not more than 1
       div.dataset.alpha = Math.min(parseFloat(div.dataset.alpha) + 0.1, 1);
-      div.style.backgroundColor = `rgba(0, 0, 0, ${div.dataset.alpha})`;
+
+      // Set the background color with the new RGB values and alpha
+      div.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${div.dataset.alpha})`;
     });
 
     container.appendChild(div);
